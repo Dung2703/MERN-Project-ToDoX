66f53cb99f0d178b5fc260f60d37c465c980d46a
Tri Dung <ttdung10x@gmail.com>
2026-03-08 14:55:28 +0700

    add method

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,11 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
+});
+
+app.get("/api/task", (request, response) => {
+    response.send("task to do");
 });
