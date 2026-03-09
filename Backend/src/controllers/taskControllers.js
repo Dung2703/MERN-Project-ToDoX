dce8fc6ac0c90045e9311de0971c35c4ac1d2270
Tri Dung <ttdung10x@gmail.com>
2026-03-09 11:56:08 +0700

    clear backend

--- Backend/src/controllers/taskControllers.js
+++ Backend/src/controllers/taskControllers.js
@@ -1,15 +1,51 @@
-export const getAllTasks = (request, response) => {
-    response.status(200).send("50 task to do");
+import Task from "../model/Task.js";
+
+
+export const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find().sort({ createdAt: -1 });
+        res.status(200).json(tasks);
+    } catch (error) {
+        console.log("Error fetching tasks:", error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const createTask = (request, response) => {
-    response.status(201).json({ message: "Task created successfully" });
+export const createTask = async (req, res) => {
+    try {
+        const { title } = req.body;
+        const task = new Task({ title });
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.log("Error creating task:", error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const updateTask = (request, response) => {
-    response.status(200).json({ message: "Task updated successfully" });
+export const updateTask = async (req, res) => {
+    try {
+        const { title, status, completedAt } = req.body;
+        const updateTask = await Task.findByIdAndUpdate(req.params.id, { title, status, completedAt }, { new: true });
+        if (!updateTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json(updateTask);
+    } catch (error) {
+        console.log("Error updating task:", error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const deleteTask = (request, response) => {
-    response.status(200).json({ message: "Task deleted successfully" });
+export const deleteTask = async (req, res) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json({ message: "Task deleted successfully" });
+    } catch (error) {
+        console.log("Error deleting task:", error);
+        res.status(500).json({ message: error.message });
+    }
 };
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,16 +5,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-connectDB();
+
 
 const PORT = process.env.PORT || 5001;
 
-
 const app = express();
+
+app.use(express.json());
 
 app.use("/api/task", taskRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
+
 
