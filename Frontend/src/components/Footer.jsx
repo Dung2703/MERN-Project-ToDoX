e1cba4a4d99c0b659a1f8648b546ae7928575db8
Tri Dung <ttdung10x@gmail.com>
2026-03-17 22:25:01 +0700

    Footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+const Footer = ({ completedTasksCount = 0, activeTasksCount = 0 }) => {
+    return <>
+        {completedTasksCount + activeTasksCount > 0 && (
+            <div className="text-center">
+                <p className='text-sm text-muted-foreground'>
+                    {completedTasksCount > 0 && (
+                        <>
+                            You have completed {completedTasksCount} task
+                            {
+                                activeTasksCount > 0 && `, need to complete ${activeTasksCount} more.`
+                            }
+                        </>
+                    )}
+                    {completedTasksCount === 0 && activeTasksCount > 0 && (
+                        <>
+                            Start working on your tasks! You have {activeTasksCount} active taskto complete.
+
+                        </>
+
+                    )}
+                </p>
+            </div>
+        )}
+    </>
 }
 
 export default Footer
