226e3d174a90ac186028f14cf2e992ab67d38f8f
Tri Dung <ttdung10x@gmail.com>
2026-03-17 11:47:19 +0700

    Header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <div>Header</div>
-    )
+        <div className='space-y-2 text-center'>
+            <h1 className='text-4xl font-bold text-transparent bg-primary bg-clip-text'>
+                TodoX
+            </h1>
+            <p className='text-muted-foreground'>Finish The Task</p>
+        </div>
+    );
 }
 
