d04a02f261962df82fcbddc13712c17b3d563636
Tri Dung <ttdung10x@gmail.com>
2026-03-17 12:11:42 +0700

    add task

--- Frontend/src/components/AddTask.jsx
+++ Frontend/src/components/AddTask.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Card } from './ui/card';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Plus } from 'lucide-react';
 
 const AddTask = () => {
     return (
-        <div>AddTask</div>
-    )
+        <Card className="p-6 border-0 bg-gradient-card shadow-custom-lg">
+            <div className="flex flex-col gap-3 sm:flex-row">
+                <Input
+                    type="text"
+                    placeholder="Enter task title"
+                    className="h-12 text-base bg-slate-50 sm:flex-1 border-border/50 focus:border-border/50 focus:ring-20"
+                />
+                <Button variant="gradient" size="xl" classname="px-6">
+                    <Plus className='size-5' />
+                    Add
+                </Button>
+            </div>
+
+        </Card>
+    );
 }
 
-export default AddTask
+export default AddTask;
