020e574e82e46b55eb4f3878e25c7400d35ce6f5
Tri Dung <ttdung10x@gmail.com>
2026-03-17 11:41:44 +0700

    Homepage and backgroumd

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,45 @@
+import StatsAndFilter from '@/components/StatsAndFilter'
+import { Header } from '@/components/Header'
+import AddTask from '@/components/AddTask'
+import TaskList from '@/components/TaskList'
 import React from 'react'
+import TaskListPagination from '@/components/TaskListPagination'
+import DateTimeFilter from '@/components/DateTimeFilter'
+import Footer from '@/components/Footer'
 
 const HomePage = () => {
     return (
-        <div><h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1></div>
+        <div className="min-h-screen w-full bg-white relative">
+            {/* Teal Glow Background */}
+            <div
+                className="absolute inset-0 z-0"
+                style={{
+                    backgroundImage: `
+        radial-gradient(125% 125% at 50% 10%, #ffffff 40%, #14b8a6 100%)
+      `,
+                    backgroundSize: "100% 100%",
+                }}
+            />
+            {/* Your Content/Components */}
+            <div className='container pt-8 mx-auto relative z-10'>
+                <div className='w-full max-w-2xl -6 mx-auto space-y-6'>
+                    <Header />
+                    <AddTask />
+                    <TaskList />
+                    <StatsAndFilter />
+
+                    <div className='flex flex-col items-center justify-center gap-6 sm:flex-row'>
+                        <TaskListPagination />
+                        <DateTimeFilter />
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        </div>
+
+
+
+
     )
 }
 
