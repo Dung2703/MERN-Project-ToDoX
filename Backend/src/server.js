28d0b1375a3fb3758a2f0153eb457ece745f4fb4
Tri Dung <ttdung10x@gmail.com>
2026-03-22 21:47:04 +0700

    ready deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,9 +3,11 @@ import taskRoute from './route/taskRoute.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 
 dotenv.config();
-
+const __dirname = path.resolve();
 
 
 const PORT = process.env.PORT || 5001;
@@ -13,10 +15,21 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 
 // middleware
-app.use(cors({ origin: 'http://localhost:5173' }));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({ origin: 'http://localhost:5173' }));
+}
 app.use(express.json());
 
 app.use("/api/task", taskRoute);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    });
+
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
